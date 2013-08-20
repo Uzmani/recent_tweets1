3319950599911ec95f3363fc680474d6b5657381
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-08-20 10:19:05 -0700

    create new route/view for creating a new tweet

--- config/environment.rb
+++ config/environment.rb
@@ -25,23 +25,12 @@ APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
 
-# client = Twitter.configure do |config|
-#   config.consumer_key = 'nKlzlQoG8ppADM9rRiP8Q'
-#   config.consumer_secret = 'IReGX1g5QxyMAH4nTXqqVNejsXFS3J8Em7XaoaY'
-#   config.oauth_token = '23558477-EC6GYMo4zbl8vK9pb8BqEV3Lad05RtXnefxsU9dA'
-#   config.oauth_token_secret = 'mP2lFSy4ZFKz2inG3m7PocIqWvRsaK5AT9zhmsQtII'
-# end
-
-
 require APP_ROOT.join('config', 'database')
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 env_config = YAML.load_file(APP_ROOT.join('config', 'keys.yaml'))
 
-# env_config.each do |key, value|
-#   ENV[key] = value
-# end
 
 client = Twitter.configure do |config|
   config.consumer_key = env_config['YOUR_APP_CONSUMER_KEY']
@@ -49,12 +38,3 @@ client = Twitter.configure do |config|
   config.oauth_token = env_config['ACCESS_TOKEN']
   config.oauth_token_secret = env_config['ACCESS_TOKEN_SECRET']
 end
-
-
-
-
-# Set up the database and models
-# require APP_ROOT.join('config', 'database')
-
-
-# https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=twitterapi&count=2
