2f41be766f4e10dadea7424bdb1b7e4a1912b5ab
uzmani <uzmani@yahoo.com>
2013-08-20 22:37:58 -0700

    fixed heroku bug

--- config/environment.rb
+++ config/environment.rb
@@ -29,11 +29,11 @@ require APP_ROOT.join('config', 'database')
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
-env_config = YAML.load_file(APP_ROOT.join('config', 'keys.yaml'))
+# env_config = YAML.load_file(APP_ROOT.join('config', 'keys.yaml'))
 
 client = Twitter.configure do |config|
-  config.consumer_key = env_config['YOUR_APP_CONSUMER_KEY']
-  config.consumer_secret = env_config['YOUR_APP_CONSUMER_SECRET']
+  config.consumer_key = ENV['YOUR_APP_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_SECRET']
   config.oauth_token = env_config['ACCESS_TOKEN']
   config.oauth_token_secret = env_config['ACCESS_TOKEN_SECRET']
 end
